13de4b761fbebe329e6a54321b28cd885b16fbc2
abdullahraihan709-maker <abdullahraihan709@gmail.com>
2026-09-25 01:08:02 +0600

    Added 'Sort By' functionality in Listed Books

--- src/app/listed-books/page.tsx
+++ src/app/listed-books/page.tsx
@@ -3,16 +3,52 @@ import BookCard from '@/components/shared/BookCard';
 import ListedBooksCard from '@/components/shared/ListedBooksCard';
 import { Book } from '@/components/types/bookDataTypes';
 import { BooksContext } from '@/context/BooksContext';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 const ListedBooks = () => {
 
     const {readBooks, wishlist} = useContext(BooksContext);
+    const [sortBy, setSortBy] = useState< "rating" | "pages" | "year" >("rating");
+
     console.log(readBooks, wishlist, "readBooks", "wishlist");
+
+
+    const sortBooks = (books: Book[]) => {
+        const sortedBooks = [...books];
+
+        if(sortBy === "rating"){
+            sortedBooks.sort((a, b) => b.rating - a.rating);
+        } else if(sortBy === "pages"){
+            sortedBooks.sort((a, b) => b.totalPages - a.totalPages);
+        } else if(sortBy === "year"){
+            sortedBooks.sort((a, b) => b.yearOfPublishing - a.yearOfPublishing);
+        }
+
+        return sortedBooks;
+    }
+
+
+
+    const sortedReadBooks = sortBooks(readBooks); 
+    const sortedWishlist = sortBooks(wishlist);
+
 
     return (
         <div className="container mx-auto py-15" >
             <h2 className="my-7 bg-[#23BE0A] rounded-3xl py-16 font-bold text-white text-4xl text-center" >listed Books</h2>
+
+            <div className='text-center'>
+                <select value={sortBy} 
+                    onChange={(e) => setSortBy(e.target.value as "rating" | "pages" | "year" )} 
+                    defaultValue="Pick a Runtime" 
+                    className="select select-success"
+                    >
+                    <option disabled={true}>Sort By</option>
+                    <option value={"rating"} >rating</option>
+                    <option value={"pages"} >Number of Pages</option>
+                    <option value={"year"} >Published Year</option>
+                </select>
+            </div>
 
 
             {/* name of each tab group should be unique */}
@@ -21,8 +57,8 @@ const ListedBooks = () => {
 
                 <div className="tab-content bg-base-100 border-base-300 p-6">
                    
-                    { readBooks.length > 0 ? (
-                        readBooks.map((book: Book) => {
+                    { sortedReadBooks.length > 0 ? (
+                        sortedReadBooks.map((book: Book) => {
                             return <ListedBooksCard key={book.bookId} book={book} />
                         })
                      ) : (
@@ -36,8 +72,8 @@ const ListedBooks = () => {
                 
                 <div className="tab-content bg-base-100 border-base-300 p-6">
                     
-                    { wishlist.length > 0 ? (
-                        wishlist.map((book: Book) => {
+                    { sortedWishlist.length > 0 ? (
+                        sortedWishlist.map((book: Book) => {
                             return <ListedBooksCard key={book.bookId} book={book} />
                         } )
                      ) : (
